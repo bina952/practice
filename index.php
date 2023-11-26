7fc519145912df7f4a77812e8ab8e1a6e6b5be49
@bina925 <binapradhan67@gmial.com>
2023-11-26 13:28:22 +0545

    test 1

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Front Page Settings
+ * 
+ * @package Elegant_Shop_Pro
+ */
+
+if ( ! function_exists( 'elegant_shop_pro_customize_register_frontpage' ) ) :
+
+function elegant_shop_pro_customize_register_frontpage( $wp_customize ) {
+	
+    /** Front Page Settings */
+    $wp_customize->add_panel( 
+        'frontpage_settings',
+         array(
+            'priority'    => 60,
+            'capability'  => 'edit_theme_options',
+            'title'       => esc_html__( 'Front Page Settings', 'elegant-shop-pro' ),
+            'description' => esc_html__( 'Static Home Page settings.', 'elegant-shop-pro' ),
+        ) 
+    ); 
+    
+    //added thiss code
+ 
+   
+
+
+    //upto here added this extra code.
+}
+endif;
+add_action( 'customize_register', 'elegant_shop_pro_customize_register_frontpage' );
