0ed9decbd49e4fcd50d18f87800175eab71d8d04
@bina925 <binapradhan67@gmial.com>
2023-11-26 13:39:36 +0545

    tesaf

--- index.php
+++ index.php
@@ -13,13 +13,8 @@ function elegant_shop_pro_customize_register_frontpage( $wp_customize ) {
     $wp_customize->add_panel( 
         'frontpage_settings',
          array(
-<<<<<<< HEAD
             'priority'    => 70,
             'capability'  => 'edit_theme_options', //hello
-=======
-            'priority'    => 60, //s is 
-            'capability'  => 'edit_theme_options',
->>>>>>> aee97f63eed59cadd7c0f5f8c9e5c1d572995abd
             'title'       => esc_html__( 'Front Page Settings', 'elegant-shop-pro' ),
             'description' => esc_html__( 'Static Home Page settings.', 'elegant-shop-pro' ),
         ) 
